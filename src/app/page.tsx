c8714adc8c27f3784a2f290f5881daf1246de196
José Daniel Grayson <josedanielgrayson@proton.me>
2025-11-30 16:07:25 -0500

    feat: add homepage banner

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,13 @@
 'use client';
 
+import LoadingScreen from '@/components/LoadingScreen';
 import { useApiClients } from '@/hooks/use-api-clients';
 import { Product } from '@/schemas/product';
 import { formatPrice } from '@/util';
 import { useQuery } from '@tanstack/react-query';
+import Image from 'next/image';
 import { useRouter } from 'next/navigation';
+import banner from './banner.webp';
 
 const HomePage = () => {
   const apiClients = useApiClients();
@@ -23,6 +26,15 @@ const HomePage = () => {
 
   return (
     <>
+      <div className="relative h-90 w-full">
+        <Image
+          src={banner}
+          alt="banner"
+          unoptimized
+          className="object-cover"
+          fill
+        />
+      </div>
       <main className="mx-auto max-w-4xl px-4 py-8">
         {products && (
           <>
@@ -30,30 +42,47 @@ const HomePage = () => {
               Nuestras ofertas más populares
             </h2>
 
-            <ul className="grid grid-cols-2 gap-4">
-              {products.map((product, i) => (
-                <li
-                  key={i}
-                  className="hover:bg-accent/10 cursor-pointer rounded-lg shadow-md transition"
-                  onClick={() => handleClick(product)}
-                >
-                  <div className="px-2 py-3">
-                    <h3 className="text-xl font-semibold uppercase">
-                      {product.name}
-                    </h3>
-                    <p className="text-muted my-2">{product.name}</p>
-                    <div className="flex items-end gap-x-1">
-                      <span className="text-sm">Desde</span>{' '}
-                      <div className="inline-flex min-h-7 items-center">
-                        <span className="text-accent text-xl font-semibold">
-                          {formatPrice(product.price)}
-                        </span>
+            <div className="grid grid-cols-2 gap-4">
+              {!data ? (
+                <LoadingScreen />
+              ) : (
+                products.map((product, i) => (
+                  <li
+                    key={i}
+                    className="hover:bg-accent/10 cursor-pointer overflow-hidden rounded-lg shadow-md transition"
+                    onClick={() => handleClick(product)}
+                  >
+                    <div>
+                      <div className="relative mb-7 h-40 w-full bg-neutral-200">
+                        {product.image_url && (
+                          <Image
+                            src={product.image_url}
+                            unoptimized
+                            alt="product"
+                            fill
+                            className="object-cover"
+                          />
+                        )}
+                      </div>
+                      <div className="px-2 py-3">
+                        <h3 className="text-xl font-semibold uppercase">
+                          {product.name}
+                        </h3>
+                        <p className="text-muted my-2">{product.name}</p>
+                        <div className="flex items-end gap-x-1">
+                          <span className="text-sm">Desde</span>{' '}
+                          <div className="inline-flex min-h-7 items-center">
+                            <span className="text-accent text-xl font-semibold">
+                              {formatPrice(product.price)}
+                            </span>
+                          </div>
+                        </div>
                       </div>
                     </div>
-                  </div>
-                </li>
-              ))}
-            </ul>
+                  </li>
+                ))
+              )}
+            </div>
           </>
         )}
       </main>
