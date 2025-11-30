33e1065398154f91100a504269be54554bc22ce7
José Daniel Grayson <josedanielgrayson@proton.me>
2025-11-30 03:39:05 -0500

    feat: hide attention panel for admins

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,22 +1,28 @@
+'use client';
+
 import { brixtonWood } from '@/fonts';
+import { useAuth } from '@/hooks/use-auth';
 import Link from 'next/link';
 import { LuCar, LuNewspaper } from 'react-icons/lu';
 import { twJoin } from 'tailwind-merge';
 
 const DashboardPage = () => {
+  const auth = useAuth();
   return (
     <main className="mx-auto max-w-4xl py-12">
       <h1 className={twJoin(brixtonWood.className, 'mb-8 text-6xl uppercase')}>
         Dashboard
       </h1>
       <nav className="wrap flex justify-center gap-x-8 gap-y-3 text-xl">
-        <Link
-          href="/dashboard/panel"
-          className="bg-accent text-background rounded px-6 py-3"
-        >
-          <LuCar className="mr-2 inline" />
-          Panel de atención
-        </Link>
+        {auth.user && auth.user.role !== 'admin' && (
+          <Link
+            href="/dashboard/panel"
+            className="bg-accent text-background rounded px-6 py-3"
+          >
+            <LuCar className="mr-2 inline" />
+            Panel de atención
+          </Link>
+        )}
         <Link
           href="/dashboard/orders"
           className="bg-accent text-background rounded px-6 py-3"
