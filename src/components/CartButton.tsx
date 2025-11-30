38e6952f29585ed2aa44be59200cbb12abf47ebc
dfigueroaw <dfigueroaw@gmail.com>
2025-11-30 14:49:52 -0500

    feat: updated shopping cart icon

--- src/components/CartButton.tsx
+++ src/components/CartButton.tsx
@@ -1,6 +1,7 @@
 'use client';
 
 import { useCart } from '@/hooks/use-cart';
+import { ShoppingCart } from 'lucide-react';
 import { useState } from 'react';
 import CartDrawer from './CartDrawer';
 
@@ -14,7 +15,8 @@ const CartButton = () => {
         onClick={() => setOpen(true)}
         className="relative rounded-md px-3 py-2 hover:bg-black/10"
       >
-        🛒
+        <ShoppingCart className="h-5 w-5" />
+
         {totalItems > 0 && (
           <span className="absolute -top-1 -right-1 rounded-full bg-black px-1 text-xs text-white">
             {totalItems}
